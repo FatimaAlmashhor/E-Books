a94227d78af9ee3aabd5b6729e5fe7efdc1d08ab
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-01-20 14:56:03 +0300

    Feat : cart and filter feature

---
+++ src/js/search.js
@@ -0,0 +1,130 @@
+const get_one = (element) => document.querySelector(element);
+const get_all = (element) => document.querySelectorAll(element);
+const product_list = get_one('#product_list')
+const cart_counter = get_one('#cart-counter')
+const damoData = [
+    {
+        id: 0,
+        name: 'لانك الله',
+        title: 'كتاب الكتروني',
+        price: "19.22",
+        img: "../assets/images/465531.jpg",
+    },
+    {
+        id: 0,
+        title: 'كتاب الكتروني',
+        name: 'كلي ونامي ',
+        price: "19.22",
+        img: "../assets/images/465531.jpg",
+    },
+    {
+        id: 0,
+        title: 'كتاب الكتروني',
+        name: ' نون النسوه',
+        price: "19.22",
+        img: "../assets/images/465531.jpg",
+    },
+    {
+        id: 0,
+        title: 'كتاب الكتروني',
+        name: 'تخاريف  ',
+        price: "19.22",
+        img: "../assets/images/465531.jpg",
+    },
+    {
+        id: 0,
+        title: 'كتاب الكتروني',
+        name: 'علم النفس  ',
+        price: "19.22",
+        img: "../assets/images/465531.jpg",
+    },
+    {
+        id: 0,
+        title: 'كتاب الكتروني',
+        name: 'علم النفس  ',
+        price: "19.22",
+        img: "../assets/images/465531.jpg",
+    },
+
+]
+
+let current_counter = 0;
+window.onload = () => {
+    _fetchData(damoData);
+    // add to cart function 
+    let storeCart = localStorage.getItem('cart_counter');
+    console.log('[storeCart]', storeCart);
+    storeCart = storeCart === null ? 0 : storeCart;
+    current_counter = storeCart;
+    cart_counter.innerText = current_counter > 9 ? "9+" : current_counter;
+    get_all('.cart_icon').forEach(element => {
+        element.addEventListener('click', (e) => {
+            current_counter++;
+            cart_counter.innerText = current_counter > 9 ? "9+" : current_counter;
+            localStorage.setItem('cart_counter', current_counter)
+        })
+    })
+
+}
+
+const _fetchData = (arr) => {
+    arr.forEach(element => {
+        let li_element = document.createElement('li');
+        li_element.innerHTML = `<div  class="product_warpper flex-col">
+    <div class='product_img'>
+      <img src=${element.img} alt="">
+    </div>
+    <div class="product_content">
+      <div class="product_name">
+        <h4 class="font-bold">${element.title}</h4>
+        <!-- <p>something</p> -->
+        <p class='color-gray font-14'>${element.name}</p>
+      </div>
+      <div class="product_price color-red">
+        <h3 class="font-bold">${element.price}<small>ر.ي</small></h3>
+        <p class="font-12 color-gray">شامل الضريبه</p>
+      </div>
+      <!-- <div class="product_more-info">
+        inforamtion
+      </div> -->
+      <div class="product_interactive flex-row  justify_content_around ">
+        <span class="w-20  flex-center ">
+          <img src="../assets/svgs/star.svg" />
+        </span>
+        <span class="cart_icon adding-cart w-20">
+          <img src="../assets/svgs/cart.svg" />
+        </span>
+        <span class="w-20">
+          <img src="../assets/svgs/two-arrow.svg" />
+        </span>
+        
+      </div>
+    </div>
+  </div>`;
+        li_element.classList.add('product')
+        product_list.append(li_element);
+    })
+    console.log('i am the loop element', arr);
+}
+// get_one('#search_btn').addEventListener('click', (e) => {
+
+// })
+// document.addEventListener('keydown')
+get_one('#search_input').addEventListener('keydown', async (e) => {
+    product_list.innerHTML = '';
+    console.log(e.target.value);
+    let filter = damoData.filter(element => {
+        if (element.name.includes(e.target.value))
+            return element
+    })
+
+    console.log('[filter ] , ', filter);
+    await _fetchData(filter);
+
+    if (e.target.value.length === '') {
+        _fetchData(damoData)
+    }
+})
+
+
+
---
+++ utils/damoData.js
@@ -0,0 +1,169 @@
+<li class="product">
+<span class=" discount flex-row justify_content_between font-14">
+  <span class="count-down"> </span>
+  <span class="font-bold">OFF</span>
+</span>
+<a href='../pages/details.html' class="product_warpper flex-col">
+  <div class="product_img">
+    <img src="../assets/images/465531.jpg" alt="">
+  </div>
+  <div class="product_content">
+    <div class="product_name">
+      <h4 class="">كتاب التروني</h4>
+      <!-- <p>something</p> -->
+    </div>
+    <div class="product_price color-red">
+      <h3 class="font-bold">19.99<small>ر.ي</small></h3>
+      <p class="font-12 color-gray">شامل الضريبه</p>
+    </div>
+    <!-- <div class="product_more-info">
+      inforamtion
+    </div> -->
+    <div class="product_interactive flex-row  justify_content_around ">
+      <span class="w-20  flex-center ">
+        <img src="../assets/svgs/star.svg" />
+      </span>
+      <span class=" w-20 adding-cart">
+        <img src="../assets/svgs/cart.svg" />
+      </span>
+      <span class="w-20">
+        <img src="../assets/svgs/two-arrow.svg" />
+      </span>
+      
+    </div>
+  </div>
+</a>
+</li>
+<li class="product">
+<span class=" discount flex-row justify_content_between font-14">
+  <span class="count-down"> </span>
+  <span class="font-bold">OFF</span>
+</span>
+<a href='../pages/details.html' class="product_warpper flex-col">
+  <div class="product_img">
+    <img src="../assets/images/578420.jpg" alt="">
+  </div>
+  <div class="product_content">
+    <div class="product_name">
+      <h4 class="">كتاب التروني</h4>
+      <!-- <p>something</p> -->
+    </div>
+    <div class="product_price color-red">
+      <h3 class="font-bold">19.99<small>ر.ي</small></h3>
+      <p class="font-12 color-gray">شامل الضريبه</p>
+    </div>
+    <!-- <div class="product_more-info">
+      inforamtion
+    </div> -->
+    <div class="product_interactive flex-row  justify_content_around ">
+      <span class="w-20  flex-center ">
+        <img src="../assets/svgs/star.svg" />
+      </span>
+      <span class="adding-cart w-20">
+        <img src="../assets/svgs/cart.svg" />
+      </span>
+      <span class="w-20">
+        <img src="../assets/svgs/two-arrow.svg" />
+      </span>
+      
+    </div>
+  </div>
+</a>
+</li>
+<li class="product">
+<a href='../pages/details.html' class="product_warpper flex-col">
+  <div class="product_img">
+    <img src="../assets/images/561080.jpg" alt="">
+  </div>
+  <div class="product_content">
+    <div class="product_name">
+      <h4 class="">كتاب التروني</h4>
+      <!-- <p>something</p> -->
+    </div>
+    <div class="product_price color-red">
+      <h3 class="font-bold">19.99<small>ر.ي</small></h3>
+      <p class="font-12 color-gray">شامل الضريبه</p>
+    </div>
+    <!-- <div class="product_more-info">
+      inforamtion
+    </div> -->
+    <div class="product_interactive flex-row  justify_content_around ">
+      <span class="w-20  flex-center ">
+        <img src="../assets/svgs/star.svg" />
+      </span>
+      <span class="adding-cart w-20">
+        <img src="../assets/svgs/cart.svg" />
+      </span>
+      <span class="w-20">
+        <img src="../assets/svgs/two-arrow.svg" />
+      </span>
+      
+    </div>
+  </div>
+</a>
+</li>
+<li class="product">
+<a href='../pages/details.html' class="product_warpper flex-col">
+  <div class="product_img">
+    <img src="../assets/images/504021.jpg" alt="">
+  </div>
+  <div class="product_content">
+    <div class="product_name">
+      <h4 class="">كتاب التروني</h4>
+      <!-- <p>something</p> -->
+    </div>
+    <div class="product_price color-red">
+      <h3 class="font-bold">19.99<small>ر.ي</small></h3>
+      <p class="font-12 color-gray">شامل الضريبه</p>
+    </div>
+    <!-- <div class="product_more-info">
+      inforamtion
+    </div> -->
+    <div class="product_interactive flex-row  justify_content_around ">
+      <span class="w-20  flex-center ">
+        <img src="../assets/svgs/star.svg" />
+      </span>
+      <span class="adding-cart w-20">
+        <img src="../assets/svgs/cart.svg" />
+      </span>
+      <span class="w-20">
+        <img src="../assets/svgs/two-arrow.svg" />
+      </span>
+      
+    </div>
+  </div>
+</a>
+</li>
+<li class="product">
+
+<a href='../pages/details.html' class="product_warpper flex-col">
+  <div class="product_img">
+    <img src="../assets/images/465531.jpg" alt="">
+  </div>
+  <div class="product_content">
+    <div class="product_name">
+      <h4 class="">كتاب التروني</h4>
+      <!-- <p>something</p> -->
+    </div>
+    <div class="product_price color-red">
+      <h3 class="font-bold">19.99<small>ر.ي</small></h3>
+      <p class="font-12 color-gray">شامل الضريبه</p>
+    </div>
+    <!-- <div class="product_more-info">
+      inforamtion
+    </div> -->
+    <div class="product_interactive flex-row  justify_content_around ">
+      <span class="w-20  flex-center ">
+        <img src="../assets/svgs/star.svg" />
+      </span>
+      <span class="adding-cart w-20">
+        <img src="../assets/svgs/cart.svg" />
+      </span>
+      <span class="w-20">
+        <img src="../assets/svgs/two-arrow.svg" />
+      </span>
+      
+    </div>
+  </div>
+</a>
+</li>
