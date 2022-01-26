b3f62e0641cc1b46a2c3e14a48ec0b2725838cff
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-01-26 18:47:13 +0300

    Feat : localStorge lnag;

--- src/js/toggleLangs.js
+++ src/js/toggleLangs.js
@@ -1,9 +1,13 @@
 const switch_lang = document.getElementById('switch_lang');
 
+let cuurentLang = localStorage.getItem('lang')
 switch_lang.addEventListener('click', (e) => {
     const body = document.getElementsByTagName('body')
+    cuurentLang = localStorage.getItem('lang')
+    cuurentLang === null ? 'ar' : cuurentLang
+    let switchLang = cuurentLang === 'ar' ? 'en' : 'ar'
     body[0].classList.toggle('en')
-    e.target.innerText = body[0].className == '' ? 'english' : 'العربية'
-    console.log('body.classList', body[0].className);
+    localStorage.setItem('lang', switchLang)
+    e.target.innerText = switchLang === 'en' ? 'english' : 'العربية'
 
 })
